1fd97927cde34f6a3e2865dcf45af7bbe26e447e
dominionolonilebi <dominionolufunke27@gmail.com>
2024-12-21 13:27:40 +0100

    updated

--- server.js
+++ server.js
@@ -74,12 +74,12 @@ app.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid Email Address" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Password Incorrect" });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
